6298b66a32156831e949e5adcf62594d6cabb9f9
ACHIEVERS\ben.white <ben.white@achievers.com>
2016-11-01 12:53:41 -0400

    working

--- extension/api.js
+++ extension/api.js
@@ -1,220 +1,226 @@
 (() => {
-	'use strict';
+  'use strict';
 
-	window.GitHubNotify = (() => {
-		const defaults = {
-			rootUrl: 'https://api.github.com/',
-			oauthToken: '',
-			useParticipatingCount: false,
-			interval: 60
-		};
+  window.GitHubNotify = (() => {
+    const defaults = {
+      rootUrl: 'https://api.github.com/',
+      oauthToken: '',
+      useParticipatingCount: false,
+      interval: 60
+    };
 
-		const api = {
-			settings: {
-				get: name => {
-					//console.log("name " + name)
-					const item = localStorage.getItem(name);
-					//console.log("item " + item)
+    const api = {
+      settings: {
+        get: name => {
+          //console.log("name " + name)
+          const item = localStorage.getItem(name);
+          //console.log("item " + item)
 
-					if (item === null) {
-						return {}.hasOwnProperty.call(defaults, name) ? defaults[name] : undefined;
-					}
+          if (item === null) {
+            return {}.hasOwnProperty.call(defaults, name) ? defaults[name] : undefined;
+          }
 
-					if (item === 'true' || item === 'false') {
-						return item === 'true';
-					}
+          if (item === 'true' || item === 'false') {
+            return item === 'true';
+          }
 
-					return item;
-				},
-				set: localStorage.setItem.bind(localStorage),
-				remove: localStorage.removeItem.bind(localStorage),
-				reset: localStorage.clear.bind(localStorage)
-			}
-		};
-
-		api.defaults = defaults;
-
-		return api;
-	})();
-
-	window.GitHubNotify.requestPermission = permission => {
-//console.log("requestPermission")
-		return new Promise(resolve => {
-			chrome.permissions.request({
-				permissions: [permission]
-			}, granted => {
-				window.GitHubNotify.settings.set(`${permission}_permission`, granted);
-				resolve(granted);
-			});
-		});
-	};
-
-	window.GitHubNotify.queryPermission = permission => {
-//console.log("queryPermission")
-		return new Promise(resolve => {
-			chrome.permissions.contains({
-				permissions: [permission]
-			}, resolve);
-		});
-	};
-
-	window.GitHubNotify.request = url => {
-//console.log("request")
-		const token = window.GitHubNotify.settings.get('oauthToken');
-		if (!token) {
-			return Promise.reject(new Error('missing token'));
-		}
-
-		/* eslint-disable quote-props */
-		const headers = Object.assign({
-			Authorization: `token ${token}`,
-			'If-Modified-Since': ''
-		});
-		/* eslint-enable quote-props */
-
-		return fetch(url, {headers});
-	};
-
-	window.GitHubNotify.getApiUrl = (repo) => {
-//console.log("getApiUrl")
-		const rootUrl = window.GitHubNotify.settings.get('rootUrl');
-
-		if (/(^(https:\/\/)?(api\.)?github\.com)/.test(rootUrl)) {
-			return 'https://api.github.com/repos/'+repo+'/pulls';
-		}
-		return `${rootUrl}api/v3/repos/`+repo+`/pulls`;
-	};
-
-//https://github.corp.achievers.com/api/v3/repos/BE/PFA/pulls?access_token=9b03b8b48a278f91575387a5351b7ee77ae88341
-
-	window.GitHubNotify.getTabUrl = () => {
-//console.log("getTabUrl")
-		let rootUrl = window.GitHubNotify.settings.get('rootUrl');
-
-		if (/api.github.com\/$/.test(rootUrl)) {
-			rootUrl = 'https://github.com/';
-		}
-
-		const tabUrl = `${rootUrl}notifications`;
-		if (window.GitHubNotify.settings.get('useParticipatingCount')) {
-			return `${tabUrl}/participating`;
-		}
-		return tabUrl;
-	};
-window.GitHubNotify.buildQuery = options => {
-  //console.log("buildQuery")
-  const perPage = options.perPage;
-  const query = [`per_page=${perPage}`];
-  if (window.GitHubNotify.settings.get('useParticipatingCount')) {
-    query.push('participating=true');
-  }
-  return query;
-};
-
-
-
-
-window.gitHubNotifCount = (repositories) => {
-
-  let urls = [];
-  var prCount, currentRepo, hiddenPRs, reducedPRs, teamMates, myId, list;
-
-
-  repositories.forEach(repo => {
-    const url = `${window.GitHubNotify.getApiUrl(repo)}`;
-    urls.push(url);
-  }); //PR forEach
-
-  const grabContent = url => window.GitHubNotify.request(url)
-    .then(res => res.json().then(repoData => {
-
-      prCount = repoData.length
-
-      hiddenPRs = window.GitHubNotify.settings.get('hiddenPRs');
-      if (hiddenPRs !== undefined && hiddenPRs.length > 0) {
-        hiddenPRs = JSON.parse(hiddenPRs);
+          return item;
+        },
+        set: localStorage.setItem.bind(localStorage),
+        remove: localStorage.removeItem.bind(localStorage),
+        reset: localStorage.clear.bind(localStorage)
       }
+    };
+
+    api.defaults = defaults;
+
+    return api;
+  })();
+
+  window.GitHubNotify.requestPermission = permission => {
+    //console.log("requestPermission")
+    return new Promise(resolve => {
+      chrome.permissions.request({
+        permissions: [permission]
+      }, granted => {
+        window.GitHubNotify.settings.set(`${permission}_permission`, granted);
+        resolve(granted);
+      });
+    });
+  };
+
+  window.GitHubNotify.queryPermission = permission => {
+    //console.log("queryPermission")
+    return new Promise(resolve => {
+      chrome.permissions.contains({
+        permissions: [permission]
+      }, resolve);
+    });
+  };
+
+  window.GitHubNotify.request = url => {
+    //console.log("request")
+    const token = window.GitHubNotify.settings.get('oauthToken');
+    if (!token) {
+      return Promise.reject(new Error('missing token'));
+    }
+
+    /* eslint-disable quote-props */
+    const headers = Object.assign({
+      Authorization: `token ${token}`,
+      'If-Modified-Since': ''
+    });
+    /* eslint-enable quote-props */
+
+    return fetch(url, { headers });
+  };
+
+  window.GitHubNotify.getApiUrl = (repo) => {
+    //console.log("getApiUrl")
+    const rootUrl = window.GitHubNotify.settings.get('rootUrl');
+
+    if (/(^(https:\/\/)?(api\.)?github\.com)/.test(rootUrl)) {
+      return 'https://api.github.com/repos/' + repo + '/pulls';
+    }
+    return `${rootUrl}api/v3/repos/` + repo + `/pulls`;
+  };
 
 
-      reducedPRs = _(repoData).filter(function(prs) {
-        return !_(hiddenPRs).contains(prs.id);
-      }, 0);
+  window.GitHubNotify.getTabUrl = () => {
+    //console.log("getTabUrl")
+    let rootUrl = window.GitHubNotify.settings.get('rootUrl');
+
+    if (/api.github.com\/$/.test(rootUrl)) {
+      rootUrl = 'https://github.com/';
+    }
+
+    const tabUrl = `${rootUrl}notifications`;
+    if (window.GitHubNotify.settings.get('useParticipatingCount')) {
+      return `${tabUrl}/participating`;
+    }
+    return tabUrl;
+  };
+  window.GitHubNotify.buildQuery = options => {
+    //console.log("buildQuery")
+    const perPage = options.perPage;
+    const query = [`per_page=${perPage}`];
+    if (window.GitHubNotify.settings.get('useParticipatingCount')) {
+      query.push('participating=true');
+    }
+    return query;
+  };
 
 
-currentRepo = window.GitHubNotify.lookup(repositories, url)
 
-      window.GitHubNotify.store('repos', currentRepo, reducedPRs)
 
-      teamMates = localStorage.getItem('teamMates') || {};
-      myId = localStorage.getItem('myId') || {};
+  window.gitHubNotifCount = () => {
 
-      if (teamMates.length > 0) {
+    let urls = [];
+    var prCount, currentRepo, hiddenPRs, reducedPRs, teamMates, myId, list;
 
-        myId = myId.split(",");
-        teamMates = teamMates.split(",");
-        list = teamMates.concat(myId);
 
-        reducedPRs = _(reducedPRs).filter(function(pr) {
-          return _(list).contains(pr.user.login);
-        });
+    const grabContent = url => window.GitHubNotify.request(url)
+      .then(res => res.json().then(repoData => {
+
+        prCount = repoData.length
+
+        hiddenPRs = window.GitHubNotify.settings.get('hiddenPRs');
+        if (hiddenPRs !== undefined && hiddenPRs.length > 0) {
+          hiddenPRs = JSON.parse(hiddenPRs);
+        }
+
+
+        reducedPRs = _(repoData).filter(function(prs) {
+          return !_(hiddenPRs).contains(prs.id);
+        }, 0);
+
+
+        currentRepo = window.GitHubNotify.lookup(repositories, url)
+
+        window.GitHubNotify.store('repos', currentRepo, reducedPRs)
+
+        teamMates = localStorage.getItem('teamMates') || {};
+        myId = localStorage.getItem('myId') || {};
+
+        if (teamMates.length > 0) {
+
+          myId = myId.split(",");
+          teamMates = teamMates.split(",");
+          list = teamMates.concat(myId);
+
+          reducedPRs = _(reducedPRs).filter(function(pr) {
+            return _(list).contains(pr.user.login);
+          });
+        };
+
+        let comments_url = [];
+        repoData.forEach(prData => {
+          comments_url.push(prData.comments_url);
+        }); //PR forEach
+
+        Promise.all(comments_url.map(subGrabContent))
+          .then(() => console.log(`subGrabContent done`))
+
+
+
+
+//    return {count: prCount, prCount: 11, interval: 60, lastModifed: 'today'};
+
+      }));
+
+
+    let comments_Data = [];
+    let subGrabContent = tempUrl => window.GitHubNotify.request(tempUrl)
+      .then(res => res.json().then(commentsData => {
+
+        comments_Data = comments_Data.concat(commentsData);
+
+        window.GitHubNotify.settings.set('comments', JSON.stringify(comments_Data));
+
+      }));
+      
+
+    let repositories = JSON.parse(window.GitHubNotify.settings.get('repositories'));
+
+    repositories.forEach(repo => {
+      const url = `${window.GitHubNotify.getApiUrl(repo)}`;
+      urls.push(url);
+    }); //PR forEach
+
+    Promise.all(urls.map(grabContent))
+      .then(() => console.log(`grabContent done`))
+
+
+
+  }; //gitHubNotifCount
+
+
+
+
+  window.GitHubNotify.lookup = (array, string) => {
+    let passBack
+    array.some(function(value) {
+      if (string.indexOf(value) >= 0) {
+        passBack = value
       };
+    });
 
-      let comments_url = [];
-      repoData.forEach(prData => {
-        comments_url.push(prData.comments_url);
-      }); //PR forEach
-
-      Promise.all(comments_url.map(subGrabContent))
-        .then(() => console.log(`subGrabContent done`))
-
-    }));
-
-
-  let comments_Data = [];
-  let subGrabContent = tempUrl => window.GitHubNotify.request(tempUrl)
-    .then(res => res.json().then(commentsData => {
-
-      comments_Data = comments_Data.concat(commentsData);
-
-      window.GitHubNotify.settings.set('comments', JSON.stringify(comments_Data));
-
-    }));
-
-  Promise.all(urls.map(grabContent))
-    .then(() => console.log(`grabContent done`))
-
-// return {count: 22, prCount: 11, interval: 60, lastModifed: 'today'};
-
-
-}; //gitHubNotifCount
+    return passBack;
+  };
 
 
 
+  window.GitHubNotify.store = (key, repo, data) => {
+    var repos, jsonText;
+    repos = window.GitHubNotify.settings.get(key) || {};
+    if (repos.length > 0) {
+      repos = JSON.parse(repos);
+    }
+    repos[repo] = data;
 
-window.GitHubNotify.lookup = (array, string) => {
-	let paddBack
-	array.some(function(value) {
-		if (string.indexOf(value) >= 0) {
-			paddBack = value
-		};
-	});
-
-	return paddBack;
-};
-
-
-
-      window.GitHubNotify.store = (key, repo, data) => {
-      	var repos, jsonText;
-      	repos = window.GitHubNotify.settings.get(key) || {};
-      	if (repos.length > 0) {
-      		repos = JSON.parse(repos);
-      	}
-      	repos[repo] = data;
-
-      	jsonText = JSON.stringify(repos);
-      	window.GitHubNotify.settings.set(key, jsonText);
-      };
+    jsonText = JSON.stringify(repos);
+    window.GitHubNotify.settings.set(key, jsonText);
+  };
 
 
 })();
--- extension/main.js
+++ extension/main.js
@@ -1,236 +1,233 @@
-(function () {
-	'use strict';
+(function() {
+  'use strict';
 
-	function render(text, color, title) {
-		chrome.browserAction.setBadgeText({text});
-		chrome.browserAction.setBadgeBackgroundColor({color});
-		chrome.browserAction.setTitle({title});
-	}
+  function render(text, color, title) {
+    chrome.browserAction.setBadgeText({ text });
+    chrome.browserAction.setBadgeBackgroundColor({ color });
+    chrome.browserAction.setTitle({ title });
+  }
 
-	function getNotificationReasonText(reason) {
-//console.log("getNotificationReasonText")
-//console.log(reason)
-		/* eslint-disable camelcase */
-		const reasons = {
-			subscribed: 'You are watching the repository',
-			manual: 'You are subscribed to this thread',
-			author: 'You created this thread',
-			comment: 'New comment',
-			mention: 'You were mentioned',
-			team_mention: 'Your team was mentioned',
-			state_change: 'Thread status changed',
-			assign: 'You were assigned to the issue',
-			default: ''
-		};
-		/* eslint-enable camelcase */
-		return reasons[reason] || reasons.default;
-	}
+  function getNotificationReasonText(reason) {
+    //console.log("getNotificationReasonText")
+    //console.log(reason)
+    /* eslint-disable camelcase */
+    const reasons = {
+      subscribed: 'You are watching the repository',
+      manual: 'You are subscribed to this thread',
+      author: 'You created this thread',
+      comment: 'New comment',
+      mention: 'You were mentioned',
+      team_mention: 'Your team was mentioned',
+      state_change: 'Thread status changed',
+      assign: 'You were assigned to the issue',
+      default: ''
+    };
+    /* eslint-enable camelcase */
+    return reasons[reason] || reasons.default;
+  }
 
-	function showDesktopNotifications(notifications, lastModifed) {
-//console.log("showDesktopNotifications")
-//console.log(notifications)
-//console.log(lastModifed)
-		const lastModifedTime = new Date(lastModifed).getTime();
+  function showDesktopNotifications(notifications, lastModifed) {
+    //console.log("showDesktopNotifications")
+    //console.log(notifications)
+    //console.log(lastModifed)
+    const lastModifedTime = new Date(lastModifed).getTime();
 
-		notifications.filter(notification => {
-			return new Date(notification.updated_at).getTime() > lastModifedTime;
-		}).forEach(notification => {
-			const notificationId = `github-notifier-${notification.id}`;
-			chrome.notifications.create(notificationId, {
-				title: notification.subject.title,
-				iconUrl: 'icon-notif-128.png',
-				type: 'basic',
-				message: notification.repository.full_name,
-				contextMessage: getNotificationReasonText(notification.reason)
-			});
+    notifications.filter(notification => {
+      return new Date(notification.updated_at).getTime() > lastModifedTime;
+    }).forEach(notification => {
+      const notificationId = `github-notifier-${notification.id}`;
+      chrome.notifications.create(notificationId, {
+        title: notification.subject.title,
+        iconUrl: 'icon-notif-128.png',
+        type: 'basic',
+        message: notification.repository.full_name,
+        contextMessage: getNotificationReasonText(notification.reason)
+      });
 
-//			window.GitHubNotify.settings.set(notificationId, notification.subject.url);
-		});
-	}
+      //			window.GitHubNotify.settings.set(notificationId, notification.subject.url);
+    });
+  }
 
-	function checkDesktopNotifications(lastModifed) {
-//console.log("checkDesktopNotifications")
-//console.log(lastModifed)
-		const query = window.GitHubNotify.buildQuery({perPage: 100});
-		const url = `${window.GitHubNotify.getApiUrl()}?${query.join('&')}`;
+  function checkDesktopNotifications(lastModifed) {
+    //console.log("checkDesktopNotifications")
+    //console.log(lastModifed)
+    const query = window.GitHubNotify.buildQuery({ perPage: 100 });
+    const url = `${window.GitHubNotify.getApiUrl()}?${query.join('&')}`;
 
-//console.log(url)
+    //console.log(url)
 
-		window.GitHubNotify.request(url).then(res => res.json()).then(notifications => {
-			showDesktopNotifications(notifications, lastModifed);
-		});
-	}
-
-
-	function handleLastModified(date) {
-//console.log("handleLastModified")
-//console.log(date)
-		let lastModifed = window.GitHubNotify.settings.get('lastModifed');
-//console.log(lastModifed)
-		const emptyLastModified = String(lastModifed) === 'null' || String(lastModifed) === 'undefined';
-		lastModifed = emptyLastModified ? new Date(0) : lastModifed;
-
-		if (date !== lastModifed) {
-			window.GitHubNotify.settings.set('lastModifed', date);
-			if (GitHubNotify.settings.get('showDesktopNotif') === true) {
-				checkDesktopNotifications(lastModifed);
-			}
-		}
-	}
-
-	function handleInterval(interval) {
-//console.log("handleInterval")
-//console.log(interval)
-		let period = 1;
-		let intervalSetting = parseInt(window.GitHubNotify.settings.get('interval'), 10);
-
-		if (typeof intervalSetting !== 'number') {
-			intervalSetting = 60;
-		}
-
-		if (interval !== null && interval !== intervalSetting) {
-			window.GitHubNotify.settings.set('interval', interval);
-			period = Math.ceil(interval / 60);
-		}
-
-		return period;
-	}
-
-	function handleError(error) {
-//console.log("handleError")
-		let symbol = '?';
-		let text;
-
-		switch (error.message) {
-			case 'missing token':
-				text = 'Missing access token, please create one and enter it in Options';
-				symbol = 'X';
-				break;
-			case 'server error':
-				text = 'You have to be connected to the internet';
-				break;
-			case 'data format error':
-			case 'parse error':
-				text = 'Unable to find count';
-				break;
-			default:
-				text = 'Unknown error';
-				break;
-		}
-
-		render(symbol, [166, 41, 41, 255], text);
-		// check again in a minute
-		scheduleAlarm(1);
-	}
-
-	function handleCount(count) {
-//console.log("handleCount")
-		if (count === 0) {
-			return '';
-		} else if (count > 9999) {
-			return '∞';
-		}
-		return String(count);
-	}
-
-	function scheduleAlarm(period) {
-//console.log("scheduleAlarm")
-//console.log(period)
-		// unconditionally schedule alarm
-		// period is in minutes
-		chrome.alarms.create({when: Date.now() + 2000 + (period * 60 * 1000)});
-	}
+    window.GitHubNotify.request(url).then(res => res.json()).then(notifications => {
+      showDesktopNotifications(notifications, lastModifed);
+    });
+  }
 
 
-function update() {
+  function handleLastModified(date) {
+    //console.log("handleLastModified")
+    //console.log(date)
+    let lastModifed = window.GitHubNotify.settings.get('lastModifed');
+    //console.log(lastModifed)
+    const emptyLastModified = String(lastModifed) === 'null' || String(lastModifed) === 'undefined';
+    lastModifed = emptyLastModified ? new Date(0) : lastModifed;
+
+    if (date !== lastModifed) {
+      window.GitHubNotify.settings.set('lastModifed', date);
+      if (GitHubNotify.settings.get('showDesktopNotif') === true) {
+        checkDesktopNotifications(lastModifed);
+      }
+    }
+  }
+
+  function handleInterval(interval) {
+    //console.log("handleInterval")
+    //console.log(interval)
+    let period = 1;
+    let intervalSetting = parseInt(window.GitHubNotify.settings.get('interval'), 10);
+
+    if (typeof intervalSetting !== 'number') {
+      intervalSetting = 60;
+    }
+
+    if (interval !== null && interval !== intervalSetting) {
+      window.GitHubNotify.settings.set('interval', interval);
+      period = Math.ceil(interval / 60);
+    }
+
+    return period;
+  }
+
+  function handleError(error) {
+    //console.log("handleError")
+    let symbol = '?';
+    let text;
+
+    switch (error.message) {
+      case 'missing token':
+        text = 'Missing access token, please create one and enter it in Options';
+        symbol = 'X';
+        break;
+      case 'server error':
+        text = 'You have to be connected to the internet';
+        break;
+      case 'data format error':
+      case 'parse error':
+        text = 'Unable to find count';
+        break;
+      default:
+        text = 'Unknown error';
+        break;
+    }
+
+    render(symbol, [166, 41, 41, 255], text);
+    // check again in a minute
+    scheduleAlarm(1);
+  }
+
+  function handleCount(count) {
+    //console.log("handleCount")
+    if (count === 0) {
+      return '';
+    } else if (count > 9999) {
+      return '∞';
+    }
+    return String(count);
+  }
+
+  function scheduleAlarm(period) {
+    //console.log("scheduleAlarm")
+    //console.log(period)
+    // unconditionally schedule alarm
+    // period is in minutes
+    chrome.alarms.create({ when: Date.now() + 2000 + (period * 60 * 1000) });
+  }
 
 
-  let repositories = JSON.parse(window.GitHubNotify.settings.get('repositories'));
+  function update() {
+
+window.gitHubNotifCount()
+
+      // .then(response => {
+      // 	const count = response.count;
+      // 	console.log("count" + count)
+
+    // 	render(handleCount(count), [65, 131, 196, 255], 'Notifier for GitHub');
+    // }).catch(handleError);
 
 
-  window.gitHubNotifCount(repositories)
-  // .then(response => {
-  // 	const count = response.count;
-  // 	console.log("count" + count)
+    //   repositories.forEach(repo => {
+    // console.log("forEach")
+    // 		window.gitHubNotifCount(repo).then(response => {
 
-  // 	render(handleCount(count), [65, 131, 196, 255], 'Notifier for GitHub');
-  // }).catch(handleError);
+    // 			const count = response.count;
+    // console.log("count" + count)
+    // 			const prCount = response.prCount;
+    // console.log("prCount" + prCount)
+    // 			const interval = response.interval;
+    // 			const lastModifed = response.lastModifed;
+    // 			const period = handleInterval(interval);
 
+    // 			scheduleAlarm(period);
+    // 			handleLastModified(lastModifed);
 
-  //   repositories.forEach(repo => {
-  // console.log("forEach")
-  // 		window.gitHubNotifCount(repo).then(response => {
-
-  // 			const count = response.count;
-  // console.log("count" + count)
-  // 			const prCount = response.prCount;
-  // console.log("prCount" + prCount)
-  // 			const interval = response.interval;
-  // 			const lastModifed = response.lastModifed;
-  // 			const period = handleInterval(interval);
-
-  // 			scheduleAlarm(period);
-  // 			handleLastModified(lastModifed);
-
-  // 			render(handleCount(count), [65, 131, 196, 255], 'Notifier for GitHub');
-  // 		}).catch(handleError);
+    // 			render(handleCount(count), [65, 131, 196, 255], 'Notifier for GitHub');
+    // 		}).catch(handleError);
 
 
 
 
-  //   }); //PR forEach
+    //   }); //PR forEach
 
 
-}
+  }
 
 
 
-	function openTab(url, tab) {
-//console.log("openTab")
-		// checks optional permissions
-		window.GitHubNotify.queryPermission('tabs').then(granted => {
-			if (granted) {
-				const currentWindow = true;
-				chrome.tabs.query({currentWindow, url}, tabs => {
-					if (tabs.length > 0) {
-						const highlighted = true;
-						chrome.tabs.update(tabs[0].id, {highlighted, url});
-					} else if (tab && tab.url === 'chrome://newtab/') {
-						chrome.tabs.update(null, {url});
-					} else {
-						chrome.tabs.create({url});
-					}
-				});
-			} else {
-				chrome.tabs.create({url});
-			}
-		});
-	}
+  function openTab(url, tab) {
+    //console.log("openTab")
+    // checks optional permissions
+    window.GitHubNotify.queryPermission('tabs').then(granted => {
+      if (granted) {
+        const currentWindow = true;
+        chrome.tabs.query({ currentWindow, url }, tabs => {
+          if (tabs.length > 0) {
+            const highlighted = true;
+            chrome.tabs.update(tabs[0].id, { highlighted, url });
+          } else if (tab && tab.url === 'chrome://newtab/') {
+            chrome.tabs.update(null, { url });
+          } else {
+            chrome.tabs.create({ url });
+          }
+        });
+      } else {
+        chrome.tabs.create({ url });
+      }
+    });
+  }
 
-	chrome.alarms.create({when: Date.now() + 2000});
-	chrome.alarms.onAlarm.addListener(update);
-	chrome.runtime.onMessage.addListener(update);
+  chrome.alarms.create({ when: Date.now() + 2000 });
+  chrome.alarms.onAlarm.addListener(update);
+  chrome.runtime.onMessage.addListener(update);
 
-	// launch options page on first run
-	chrome.runtime.onInstalled.addListener(details => {
-		if (details.reason === 'install') {
-			chrome.runtime.openOptionsPage();
-		}
-	});
+  // launch options page on first run
+  chrome.runtime.onInstalled.addListener(details => {
+    if (details.reason === 'install') {
+      chrome.runtime.openOptionsPage();
+    }
+  });
 
-	chrome.browserAction.onClicked.addListener(tab => {
-		const tabUrl = window.GitHubNotify.getTabUrl();
+  chrome.browserAction.onClicked.addListener(tab => {
+    const tabUrl = window.GitHubNotify.getTabUrl();
 
-		// request optional permissions the 1rst time
-		if (window.GitHubNotify.settings.get('tabs_permission') === undefined) {
-			window.GitHubNotify.requestPermission('tabs').then(granted => {
-				window.GitHubNotify.settings.set('tabs_permission', granted);
-				openTab(tabUrl, tab);
-			});
-		} else {
-			openTab(tabUrl, tab);
-		}
-	});
+    // request optional permissions the 1rst time
+    if (window.GitHubNotify.settings.get('tabs_permission') === undefined) {
+      window.GitHubNotify.requestPermission('tabs').then(granted => {
+        window.GitHubNotify.settings.set('tabs_permission', granted);
+        openTab(tabUrl, tab);
+      });
+    } else {
+      openTab(tabUrl, tab);
+    }
+  });
 
-	update();
+  update();
 })();
